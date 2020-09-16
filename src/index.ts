029ea67873963faf828a3fa6389f4984ff07192a
Sixian <lisixian579@gmail.com>
2020-09-16 17:09:05 +0800

    show command in palette

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,36 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { Widget } from '@lumino/widgets';
 /**
  * Initialization data for the jupyterlab_apod extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-apod',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension jupyterlab-apod is activated!');
+
+    const content = new Widget();
+    const widget = new MainAreaWidget({ content });
+    widget.id = 'apod-jupyterlab';
+    widget.title.label = 'Astronomy Picture';
+    widget.title.closable = true;
+
+    const command = 'apod:open';
+    app.commands.addCommand(command, {
+      label: 'Random AP',
+      execute: () => {
+        if (!widget.isAttached) {
+          app.shell.add(widget, 'main');
+        }
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    palette.addItem({ command, category: 'Tutorial' });
   }
 };
 
